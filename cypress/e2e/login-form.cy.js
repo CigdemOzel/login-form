ce252347f6b8258977417b2f6def852eaeae9a0d
CigdemOzel <cigdemozel17@gmail.com>
2024-11-29 20:46:31 +0300

    ikinci commit

---
+++ cypress/e2e/login-form.cy.js
@@ -0,0 +1,10 @@
+describe("Register Page", () => {
+  describe("Error Messages", () => {
+    it("email input throws error for 2 chars", () => {
+      //Arrange
+      cy.visit("http://localhost:5173/");
+      //Act
+      //Assert
+    });
+  });
+});
